591c6067386b8d6f9eb804fd280190ff7cd1e364
Angel <65524105+angelllinas@users.noreply.github.com>
2023-10-17 05:41:03 -0500

    modified docker-compose

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('empleado', 'root', '', {
-    host: 'mysql', 
+const sequelize = new Sequelize('empleado', 'root', 12345, {
+    host: '192.168.3.3', 
     dialect: 'mysql',
-    port: 3306
+    port: 8000
 });
 
 
